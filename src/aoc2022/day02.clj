177ba21cf69ef84e4f362d35a7f7754f1a925648
David Wagner <wagdav@gmail.com>
2022-12-02 07:02:41 +0100

    Solve day02

--- src/aoc2022/day02.clj
+++ src/aoc2022/day02.clj
@@ -1,10 +1,53 @@
-(ns aoc2022.day02
-  (:require [clojure.string :as str]))
+(ns aoc2022.day02)
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-  (mapv #(Integer/parseInt %))))
+  (->> (re-seq #"[A-Z]" input)
+       (partition 2)))
 
-(defn solve-part1 [input])
+(def code->move {"A" :rock "B" :paper "C" :scissors
+                 "X" :rock "Y" :paper "Z" :scissors})
 
-(defn solve-part2 [input])
+(def code->outcome {"X" :loose "Y" :draw "Z" :win})
+
+(def move->score {:rock 1 :paper 2 :scissors 3})
+
+(defn game-score [opponent you]
+  (if (= you opponent)
+    3 ; draw
+    (case [you opponent]
+      [:rock :scissors] 6
+      [:scissors :paper] 6
+      [:paper :rock] 6
+      0)))
+
+(defn total-score [opponent you]
+  (+ (game-score opponent you) (move->score you)))
+
+(defn move [opponent outcome]
+  (case outcome
+    :draw  opponent
+    :win   (case opponent
+             :rock      :paper
+             :paper     :scissors
+             :scissors  :rock)
+    :loose (case opponent
+             :rock     :scissors
+             :paper    :rock
+             :scissors :paper)))
+
+(defn solve-part1 [input]
+  (reduce (fn [score [abc xyz]]
+            (let [opponent-move (code->move abc)
+                  your-move (code->move xyz)]
+              (+ score (total-score opponent-move your-move))))
+          0
+          input))
+
+(defn solve-part2 [input]
+  (reduce (fn [score [abc xyz]]
+            (let [opponent-move (code->move abc)
+                  outcome (code->outcome xyz)
+                  your-move (move opponent-move outcome)]
+              (+ score (total-score opponent-move your-move))))
+          0
+          input))
--- test/aoc2022/day02_test.clj
+++ test/aoc2022/day02_test.clj
@@ -3,14 +3,14 @@
             [aoc2022.day02 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "A Y\nB X\n C Z")
 
 (deftest works
-  (testing "with example input")
-    (is (nil? (solve-part1 example-input)))
-    (is (nil? (solve-part2 example-input)))
+  (testing "with example input"
+    (is (= 15 (solve-part1 (parse-input example-input))))
+    (is (= 12 (solve-part2 (parse-input example-input)))))
 
-  (testing "with real input")
+  (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day02.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input)))))
+      (is (= 13809 (solve-part1 input)))
+      (is (= 12316 (solve-part2 input))))))
