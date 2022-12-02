7bd9b7c107f6b2594a308be388fdf9313e6bb655
David Wagner <wagdav@gmail.com>
2022-12-02 07:14:35 +0100

    Use new name for default shell

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       let pkgs = nixpkgs.legacyPackages.${system};
       in
       {
-        devShell = pkgs.mkShell {
+        devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             clojure
             clj-kondo
