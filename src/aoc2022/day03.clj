90e8520efec150e75aaaf7a938bee691d470dca4
David Wagner <wagdav@gmail.com>
2022-12-03 08:39:56 +0100

    Solve day03

--- src/aoc2022/day03.clj
+++ src/aoc2022/day03.clj
@@ -1,10 +1,34 @@
 (ns aoc2022.day03
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-  (mapv #(Integer/parseInt %))))
+  (str/split-lines input))
 
-(defn solve-part1 [input])
+(defn char-range [start end]
+  (map char (range (int start) (inc (int end)))))
 
-(defn solve-part2 [input])
+(def priority
+  (zipmap
+    (concat (char-range \a \z)
+            (char-range \A \Z))
+    (range 1 53)))
+
+(defn solve-part1 [input]
+  (apply +
+         (map
+           (fn [line] (->> line
+                           (split-at (quot (count line) 2))
+                           (map #(into #{} %))
+                           (apply set/intersection)
+                           (first)
+                           (priority)))
+           input)))
+
+(defn solve-part2 [input]
+  (->> input
+       (map #(into #{} %))
+       (partition 3)
+       (map #(first (apply set/intersection %)))
+       (map priority)
+       (apply +)))
--- test/aoc2022/day03_test.clj
+++ test/aoc2022/day03_test.clj
@@ -3,14 +3,20 @@
             [aoc2022.day03 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example (str
+               "vJrwpWtwJgWrhcsFMMfFFhFp\n"
+               "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n"
+               "PmmdzqPrVvPwwTWBwg\n"
+               "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n"
+               "ttgJtRGJQctTZtZT\n"
+               "CrZsJsPPZsGzwwsLwLmpwMDw\n"))
 
 (deftest works
-  (testing "with example input")
-    (is (nil? (solve-part1 example-input)))
-    (is (nil? (solve-part2 example-input)))
+  (testing "with example input"
+    (is (= 157 (solve-part1 (parse-input example))))
+    (is (= 70 (solve-part2 (parse-input example)))))
 
-  (testing "with real input")
+  (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day03.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input)))))
+      (is (= 7821 (solve-part1 input)))
+      (is (= 2752 (solve-part2 input))))))
