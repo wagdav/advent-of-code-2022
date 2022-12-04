f992fd32ed3fa67edb3e06fd2888df776f9ea299
David Wagner <wagdav@gmail.com>
2022-12-04 10:17:33 +0100

    Solve day04

--- src/aoc2022/day04.clj
+++ src/aoc2022/day04.clj
@@ -1,10 +1,19 @@
-(ns aoc2022.day04
-  (:require [clojure.string :as str]))
+(ns aoc2022.day04)
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-  (mapv #(Integer/parseInt %))))
+  (->> (re-seq #"\d+" input)
+       (mapv #(Integer/parseInt %))
+       (partition 4)))
 
-(defn solve-part1 [input])
+(defn fully-overlap? [[x1 x2 y1 y2]]
+  (or (<= x1 y1 y2 x2)
+      (<= y1 x1 x2 y2)))
 
-(defn solve-part2 [input])
+(defn no-overlap? [[x1 x2 y1 y2]]
+  (or (< x2 y1) (< y2 x1)))
+
+(defn solve-part1 [input]
+  (count (filter fully-overlap? input)))
+
+(defn solve-part2 [input]
+  (count (remove no-overlap? input)))
--- test/aoc2022/day04_test.clj
+++ test/aoc2022/day04_test.clj
@@ -3,14 +3,20 @@
             [aoc2022.day04 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example
+  (str "2-4,6-8\n"
+       "2-3,4-5\n"
+       "5-7,7-9\n"
+       "2-8,3-7\n"
+       "6-6,4-6\n"
+       "2-6,4-8"))
 
 (deftest works
-  (testing "with example input")
-    (is (nil? (solve-part1 example-input)))
-    (is (nil? (solve-part2 example-input)))
+  (testing "with example input"
+    (is (= 2 (solve-part1 (parse-input example))))
+    (is (= 4 (solve-part2 (parse-input example)))))
 
-  (testing "with real input")
+  (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day04.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input)))))
+      (is (= 602 (solve-part1 input)))
+      (is (= 891 (solve-part2 input))))))
