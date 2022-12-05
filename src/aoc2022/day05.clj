f9d2a49ce2e0a16ee24beb8464a40753c1eff65c
David Wagner <wagdav@gmail.com>
2022-12-05 08:43:57 +0100

    Solve day05

--- src/aoc2022/day05.clj
+++ src/aoc2022/day05.clj
@@ -1,10 +1,39 @@
 (ns aoc2022.day05
   (:require [clojure.string :as str]))
 
+(defn transpose [m]
+  (apply mapv vector m))
+
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-  (mapv #(Integer/parseInt %))))
+  (let [[crate-block instr-block] (str/split input #"\R\R")]
+    {:crates (zipmap
+               (range 1 10)
+               (->> (str/split-lines crate-block)
+                    (map (fn [line] (->> line (drop 1) (partition 1 4) flatten)))
+                    (butlast)
+                    (transpose)
+                    (map #(remove #{\space} %))))
+     :procedure (->> (re-seq #"\d+" instr-block)
+                     (map #(Integer/parseInt %))
+                     (partition 3))}))
 
-(defn solve-part1 [input])
+(defn move-by-one [crates [amount from to]]
+  (-> crates
+    (update from #(drop amount %))
+    (update to into (take amount (crates from)))))
 
-(defn solve-part2 [input])
+(defn move-many [crates [amount from to]]
+  (-> crates
+    (update from #(drop amount %))
+    (update to into (reverse (take amount (crates from))))))
+
+(defn code [crates]
+  (apply str
+    (for [i (range 1 (inc (count crates)))]
+      (first (crates i)))))
+
+(defn solve-part1 [{:keys [crates procedure]}]
+  (code (reduce move-by-one crates procedure)))
+
+(defn solve-part2 [{:keys [crates procedure]}]
+  (code (reduce move-many crates procedure)))
--- test/aoc2022/day05_test.clj
+++ test/aoc2022/day05_test.clj
@@ -3,14 +3,24 @@
             [aoc2022.day05 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example
+  (str
+    "    [D]    \n"
+    "[N] [C]    \n"
+    "[Z] [M] [P]\n"
+    " 1   2   3 \n"
+    "\n"
+    "move 1 from 2 to 1\n"
+    "move 3 from 1 to 3\n"
+    "move 2 from 2 to 1\n"
+    "move 1 from 1 to 2\n"))
 
 (deftest works
-  (testing "with example input")
-    (is (nil? (solve-part1 example-input)))
-    (is (nil? (solve-part2 example-input)))
+  (testing "with example input"
+    (is (= "CMZ" (solve-part1 (parse-input example))))
+    (is (= "MCD" (solve-part2 (parse-input example)))))
 
-  (testing "with real input")
+  (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day05.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input)))))
+      (is (= "LBLVVTVLP" (solve-part1 input)))
+      (is (= "TPFFBDRJD" (solve-part2 input))))))
