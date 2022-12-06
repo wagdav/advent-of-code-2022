60de7e435758a2a98d7d056dd3ba0eab7819d6e9
David Wagner <wagdav@gmail.com>
2022-12-06 06:35:19 +0100

    Solve day06

--- src/aoc2022/day06.clj
+++ src/aoc2022/day06.clj
@@ -1,10 +1,14 @@
-(ns aoc2022.day06
-  (:require [clojure.string :as str]))
+(ns aoc2022.day06)
 
-(defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-  (mapv #(Integer/parseInt %))))
+(defn start-of-unique [n input]
+  (->> (partition n 1 input)
+       (map-indexed #(vector %1 (distinct %2)))
+       (drop-while #(not= n (count (second %))))
+       (ffirst)
+       (+ n)))
 
-(defn solve-part1 [input])
+(defn solve-part1 [input]
+  (start-of-unique 4 input))
 
-(defn solve-part2 [input])
+(defn solve-part2 [input]
+  (start-of-unique 14 input))
--- test/aoc2022/day06_test.clj
+++ test/aoc2022/day06_test.clj
@@ -1,16 +1,16 @@
 (ns aoc2022.day06-test
   (:require [clojure.test :refer [deftest is testing]]
-            [aoc2022.day06 :refer [parse-input solve-part1 solve-part2]]
+            [aoc2022.day06 :refer [solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example "mjqjpqmgbljsphdztnvjfqwrcgsmlb")
 
 (deftest works
-  (testing "with example input")
-    (is (nil? (solve-part1 example-input)))
-    (is (nil? (solve-part2 example-input)))
+  (testing "with example input"
+    (is (= 7 (solve-part1 example)))
+    (is (= 19 (solve-part2 example))))
 
-  (testing "with real input")
-    (let [input (parse-input (slurp (io/resource "day06.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input)))))
+  (testing "with real input"
+    (let [input (slurp (io/resource "day06.txt"))]
+      (is (= 1361 (solve-part1 input)))
+      (is (= 3263 (solve-part2 input))))))
