b2bc3ba78e42d4176d0cf225cc9a72a623006d0c
David Wagner <wagdav@gmail.com>
2022-12-07 07:22:15 +0100

    Solve day07

--- src/aoc2022/day07.clj
+++ src/aoc2022/day07.clj
@@ -1,10 +1,59 @@
 (ns aoc2022.day07
   (:require [clojure.string :as str]))
 
+(defn command [state [cmd-name arg]]
+  (case cmd-name
+    "ls" state
+    "cd" (case arg
+          ".." (update state :cwd #(vec (butlast %)))
+          "/"  (assoc state :cwd ["/"])
+          (update state :cwd conj arg))))
+
+(defn entry [{:keys [cwd] :as state} [size file-name]]
+  (if (= size "dir")
+    state
+    (assoc-in state (conj cwd file-name) (Integer/parseInt size))))
+
+(defn parse-line [state line]
+  (let [parts (str/split line #" ")]
+    (if (= "$" (first parts))
+      (command state (rest parts))
+      (entry state parts))))
+
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-  (mapv #(Integer/parseInt %))))
+  (reduce parse-line {:cwd ["/"]} (str/split-lines input)))
 
-(defn solve-part1 [input])
+(defn total-size [fs path]
+  (apply +
+    (for [[k s] (get-in fs path)]
+      (if (map? s)
+        (total-size fs (conj path k))
+        s))))
 
-(defn solve-part2 [input])
+(defn dirs [fs path]
+  (reduce
+    (fn [res [k s]]
+      (if (map? s)
+        (let [dir-name (conj path k)]
+          (into (conj res dir-name) (dirs fs dir-name)))
+        res))
+    []
+    (get-in fs path)))
+
+(defn solve-part1 [input]
+  (->> (dirs input ["/"])
+       (map #(total-size input %))
+       (filter #(<= % 100000))
+       (apply +)))
+
+(defn solve-part2 [input]
+  (let [disk 70000000
+        need 30000000
+        total (total-size input ["/"])
+        unused (- disk total)
+        required (- need unused)]
+     (->> (dirs input ["/"])
+          (map #(total-size input %))
+          sort
+          (drop-while #(< % required))
+          first)))
--- test/aoc2022/day07_test.clj
+++ test/aoc2022/day07_test.clj
@@ -3,14 +3,36 @@
             [aoc2022.day07 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example "$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k")
 
 (deftest works
-  (testing "with example input")
-    (is (nil? (solve-part1 example-input)))
-    (is (nil? (solve-part2 example-input)))
+  (testing "with example input"
+    (is (= 95437 (solve-part1 (parse-input example))))
+    (is (= 24933642 (solve-part2 (parse-input example)))))
 
-  (testing "with real input")
+  (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day07.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input)))))
+      (is (= 1491614 (solve-part1 input)))
+      (is (= 6400111 (solve-part2 input))))))
