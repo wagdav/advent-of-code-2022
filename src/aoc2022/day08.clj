98fc121913408650aecb289636819b419f056f4f
David Wagner <wagdav@gmail.com>
2022-12-08 08:44:32 +0100

    Solve day08

--- src/aoc2022/day08.clj
+++ src/aoc2022/day08.clj
@@ -1,10 +1,53 @@
 (ns aoc2022.day08
   (:require [clojure.string :as str]))
 
+(def example "30373
+25512
+65332
+33549
+35390")
+
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-  (mapv #(Integer/parseInt %))))
+  (let [size (count (str/split-lines input))]
+    {:size size
+     :trees (->> (re-seq #"\d" input)
+                (mapv #(Integer/parseInt %))
+                (partition size)
+                (mapv #(vec %)))}))
 
-(defn solve-part1 [input])
+(defn los [{:keys [size trees]} [row col] direction]
+ (case direction
+   :up    (for [r (range (dec row) -1 -1)] (get-in trees [r col]))
+   :down  (for [r (range (inc row) size)]  (get-in trees [r col]))
+   :right (for [c (range (inc col) size)]  (get-in trees [row c]))
+   :left  (for [c (range (dec col) -1 -1)] (get-in trees [row c]))))
 
-(defn solve-part2 [input])
+(defn visible?
+ ([grid p]
+  (or (visible? grid p :up)
+      (visible? grid p :down)
+      (visible? grid p :left)
+      (visible? grid p :right)))
+ ([{:keys [trees] :as grid} p direction]
+  (every? #(< % (get-in trees p)) (los grid p direction))))
+
+(defn score
+ ([grid p]
+  (* (score grid p :up)
+     (score grid p :down)
+     (score grid p :left)
+     (score grid p :right)))
+ ([{:keys [trees] :as grid} p direction]
+  (let [reference (get-in trees p)]
+    (reduce (fn [c t]
+              (if (< t reference)
+                (inc c)
+                (reduced (inc c))))
+            0
+            (los grid p direction)))))
+
+(defn solve-part1 [{:keys [trees size] :as grid}]
+  (count (filter true? (for [r (range size) c (range size)] (visible? grid [r c])))))
+
+(defn solve-part2 [{:keys [trees size] :as grid}]
+  (apply max (for [r (range size) c (range size)] (score grid [r c]))))
--- test/aoc2022/day08_test.clj
+++ test/aoc2022/day08_test.clj
@@ -3,14 +3,18 @@
             [aoc2022.day08 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example "30373
+25512
+65332
+33549
+35390")
 
 (deftest works
-  (testing "with example input")
-    (is (nil? (solve-part1 example-input)))
-    (is (nil? (solve-part2 example-input)))
+  (testing "with example input"
+    (is (= 21 (solve-part1 (parse-input example))))
+    (is (= 8 (solve-part2 (parse-input example)))))
 
-  (testing "with real input")
+  (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day08.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input)))))
+      (is (= 1672 (solve-part1 input)))
+      (is (= 327180 (solve-part2 input))))))
