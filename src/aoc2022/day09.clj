71c850e49fe6976e9c58411e5b1f4ea545921fc0
David Wagner <wagdav@gmail.com>
2022-12-09 07:53:36 +0100

    Solve day09

--- src/aoc2022/day09.clj
+++ src/aoc2022/day09.clj
@@ -2,9 +2,56 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-  (mapv #(Integer/parseInt %))))
+  (->> (str/split-lines input)
+       (mapv #(str/split % #" "))
+       (mapv (fn [[d n]] [d (Integer/parseInt n)]))))
 
-(defn solve-part1 [input])
+(defn expand-move [[row col] [dir amount]]
+  (reductions
+    (fn [[r c] d]
+      (case d
+        "U" [(inc r)     c]
+        "D" [(dec r)     c]
+        "L" [     r (dec c)]
+        "R" [     r (inc c)]))
+    [row col]
+    (repeat amount dir)))
 
-(defn solve-part2 [input])
+(defn one-step [x]
+  (if (pos? x) (min x 1) (max x -1)))
+
+(defn move-tail [tail head]
+  (let [delta (map - head tail)]
+    (if (every? #(<= % 1) (map abs delta))
+      tail
+      [(+ (first tail)  (one-step (first delta)))
+       (+ (second tail) (one-step (second delta)))])))
+
+(defn move [{:keys [knots visited]} m]
+  (let [new-knots (reductions
+                    #(reductions move-tail %2 %1)
+                    (expand-move (first knots) m)
+                    (next knots))]
+    {:knots (map last new-knots)
+     :visited (into visited (last new-knots))}))
+
+(defn simulate [steps size]
+  (reduce move {:knots (repeat size [0 0]) :visited #{}} steps))
+
+(defn solve-part1 [input]
+  (count (:visited (simulate input 2))))
+
+(defn solve-part2 [input]
+  (count (:visited (simulate input 10))))
+
+(defn viz [size {:keys [knots]}]
+  (let [pos->index (zipmap knots (range))]
+    (doseq [row (range size (- size) -1)]
+      (doseq [col (range (- size) (inc size))]
+        (if-let [idx (pos->index [row col])]
+          (print (char (+ 48 idx)))
+          (print ".")))
+      (println))))
+
+(comment
+  (viz 5 (simulate [["R" 4] ["U" 4] ["L" 3]] 10)))
--- test/aoc2022/day09_test.clj
+++ test/aoc2022/day09_test.clj
@@ -1,16 +1,34 @@
 (ns aoc2022.day09-test
-  (:require [clojure.test :refer [deftest is testing]]
-            [aoc2022.day09 :refer [parse-input solve-part1 solve-part2]]
+  (:require [clojure.test :refer [deftest are is testing]]
+            [aoc2022.day09 :refer [move-tail parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example "R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2")
 
 (deftest works
-  (testing "with example input")
-    (is (nil? (solve-part1 example-input)))
-    (is (nil? (solve-part2 example-input)))
+  (testing "tail movement"
+    (are [from head,   to] (= to (move-tail from head))
+         ; doesn't move
+         [0 0] [0  0], [0 0]
+         [0 0] [0  1], [0 0]
+         ; moves horizontally
+         [0 0] [0  2], [0  1]
+         [0 0] [0 -2], [0 -1]
+         ; moves diagonally
+         [0 0] [1  2], [1  1]
+         [0 0] [1 -2], [1 -1]))
 
-  (testing "with real input")
+  (testing "with example input"
+    (is (= 13 (solve-part1 (parse-input example)))))
+
+  (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day09.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input)))))
+      (is (= 5695 (solve-part1 input)))
+      (is (= 2434 (solve-part2 input))))))
