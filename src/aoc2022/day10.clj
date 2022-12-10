f2265ab71b8b4aceb4608b6307c7172067cc4038
David Wagner <wagdav@gmail.com>
2022-12-10 06:36:36 +0100

    Solve day10

--- src/aoc2022/day10.clj
+++ src/aoc2022/day10.clj
@@ -2,9 +2,32 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-  (mapv #(Integer/parseInt %))))
+ (mapv #(str/split % #" ")
+       (str/split-lines input)))
 
-(defn solve-part1 [input])
+(defn cycles [x [instr arg]]
+   (case instr
+     "noop" [x]
+     "addx" [x (+ x (Integer/parseInt arg))]))
 
-(defn solve-part2 [input])
+(defn run [program]
+  (reduce
+    (fn [res i]
+      (into res (cycles (last res) i)))
+    [1]
+    program))
+
+(defn solve-part1 [input]
+  (let [xs (run input)]
+    (apply +
+      (for [s [20 60 100 140 180 220]]
+        (* s (nth xs (dec s)))))))
+
+(defn solve-part2 [input]
+  (let [width 40
+        screen (->> (run input)
+                    (map-indexed (fn [pos x] (if (<= -1 (- (mod pos width) x) 1)
+                                                 \# \space)))
+                    (partition width))]
+    (doseq [row screen]
+      (println (str/join row)))))
--- test/aoc2022/day10_test.clj
+++ test/aoc2022/day10_test.clj
@@ -3,14 +3,12 @@
             [aoc2022.day10 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
-
 (deftest works
-  (testing "with example input")
-    (is (nil? (solve-part1 example-input)))
-    (is (nil? (solve-part2 example-input)))
+  (testing "with example input"
+    (let [input (parse-input (slurp (io/resource "day10-example.txt")))]
+      (is (= 13140 (solve-part1 input)))))
 
-  (testing "with real input")
+  (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day10.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input)))))
+      (is (= 11780 (solve-part1 input)))
+      (is (nil? (solve-part2 input))))))
