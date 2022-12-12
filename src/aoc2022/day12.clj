c7ccfde22e5d41c1661f8bcbd0859128bdeec90c
David Wagner <wagdav@gmail.com>
2022-12-12 08:09:07 +0100

    Solve day12 part1

--- src/aoc2022/day12.clj
+++ src/aoc2022/day12.clj
@@ -1,10 +1,69 @@
 (ns aoc2022.day12
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.data.priority-map :refer [priority-map]]))
+
+(def example "Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi")
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-  (mapv #(Integer/parseInt %))))
+  (str/split-lines input))
 
-(defn solve-part1 [input])
+(def ex (parse-input example))
+
+(defn at [heighthmap [x y]]
+  (get-in heighthmap [x y]))
+
+(defn find-start-stop [heightmap]
+  (into {}
+    (let [rows (count heightmap)
+          cols (count (first heightmap))]
+      (for [x (range (inc rows))
+            y (range (inc cols))
+            :when (#{\S \E} (at heightmap [x y]))]
+        [(at heightmap [x y]) [x y]]))))
+
+(defn height->int [h]
+  (case h
+    \S (height->int \a)
+    \E (height->int \z)
+    (int (first (str/lower-case h)))))
+
+(defn moves [heightmap from]
+  (let [directions '([0 1] [0 -1] [1 0] [-1 0])]
+    (filter
+      (fn [to]
+        (and (at heightmap to)
+             (>= 1
+              (- (height->int (at heightmap to))
+                 (height->int (at heightmap from))))))
+      (for [d directions]
+        (mapv + from d)))))
+
+(defn shortest-path [heightmap start end]
+  (loop [frontier (priority-map start 0)
+         explored #{}]
+    (let [[current distance] (peek frontier)]
+      (if (= current end)
+        distance
+        (recur
+          (reduce
+             (fn [queue [point tentative-distance]]
+               (assoc queue point (min tentative-distance
+                                       (get queue point tentative-distance))))
+
+             (pop frontier)
+
+             (->> (moves heightmap current)
+                  (remove explored)
+                  (map #(vector % (inc distance)))))
+
+          (conj explored current))))))
+
+(defn solve-part1 [input]
+  (let [s (find-start-stop input)]
+    (shortest-path input (s \S) (s \E))))
 
 (defn solve-part2 [input])
--- test/aoc2022/day12_test.clj
+++ test/aoc2022/day12_test.clj
@@ -3,14 +3,18 @@
             [aoc2022.day12 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example "Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi")
 
 (deftest works
-  (testing "with example input")
-    (is (nil? (solve-part1 example-input)))
-    (is (nil? (solve-part2 example-input)))
+  (testing "with example input"
+    (is (= 31 (solve-part1 (parse-input example))))
+    (is (nil? (solve-part2 (parse-input example)))))
 
-  (testing "with real input")
+  (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day12.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input)))))
+        (is (= 380 (solve-part1 input)))
+        (is (nil? (solve-part2 input))))))
