b2916c5191a1492503d9404a314cb497ab92c418
David Wagner <wagdav@gmail.com>
2022-12-14 08:49:14 +0100

    Solve day14

--- src/aoc2022/day14.clj
+++ src/aoc2022/day14.clj
@@ -1,10 +1,110 @@
 (ns aoc2022.day14
   (:require [clojure.string :as str]))
 
+(def sand-source [500 0])
+
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-  (mapv #(Integer/parseInt %))))
+  (->> (str/split-lines input)
+       (mapv #(re-seq #"-?\d+" %))
+       (mapv (fn [line] (mapv #(Integer/parseInt %) line)))
+       (mapv #(partition 2 %))))
 
-(defn solve-part1 [input])
+(defn unit [x]
+  (if (pos? x) (min 1 x) (max -1 x)))
 
-(defn solve-part2 [input])
+(defn draw-wall [path]
+  (conj
+    (apply concat
+      (for [[from to] (partition 2 1 path)]
+        (let [delta (map (comp unit -) to from)]
+          (take-while (partial not= to) (iterate #(map + delta %) from)))))
+    (last path)))
+
+(defn initial-state [input]
+  (let [walls (->> input (map draw-wall) (apply concat) set)
+        [left right] (->> walls (map first) (apply (juxt min max)))
+        depth (->> walls (map second) (apply max))]
+    {:wall? walls
+     :left left
+     :right right
+     :depth depth
+     :sand? #{}
+     :floor false}))
+
+(defn add-floor [state]
+  (-> state
+      (update :depth inc)
+      (assoc :floor true)))
+
+(defn fall [{:keys [wall? sand? depth]} [x0 y0]]
+  (loop [y y0]
+    (if (or (wall? [x0 (inc y)])
+            (sand? [x0 (inc y)])
+            (>= y depth))
+     [x0 y]
+     (recur (inc y)))))
+
+(defn roll [{:keys [sand? wall? floor depth]} from]
+  (remove
+    (fn [[_x y :as p]] (or (wall? p)
+                          (sand? p)
+                          (and floor (< depth y))))
+    (map (partial mapv + from)
+         [[-1 1] [1 1]])))
+
+(defn sand [{:keys [floor left right] :as state}]
+  (loop [[x _y :as pos] (fall state sand-source)]
+    (let [moves (roll state pos)]
+      (cond
+       (empty? moves)
+       (update state :sand? conj pos)
+
+       (and (not floor) (or (< x left) (< right x)))
+       (assoc state :outflow true)
+
+       :else
+       (recur (fall state (first moves)))))))
+
+(defn units-of-sand [{:keys [sand?]}]
+  (count sand?))
+
+(defn simulate-until [pred start]
+  (->> (iterate sand start)
+       (drop-while (complement pred))
+       first))
+
+(defn flowing-into-the-abyss? [{:keys [outflow]}]
+  (boolean outflow))
+
+(defn source-is-blocked? [{:keys [sand?]}]
+  (sand? sand-source))
+
+(defn solve-part1 [input]
+  (->> (initial-state input)
+       (simulate-until flowing-into-the-abyss?)
+       units-of-sand))
+
+(defn solve-part2 [input]
+  (->> (initial-state input)
+       add-floor
+       (simulate-until source-is-blocked?)
+       units-of-sand))
+
+(defn viz [{:keys [left right depth sand? wall?]}]
+  (let [[xmin xmax] (->> sand? (map first) (apply (juxt min max)))]
+    (doseq [row (range 0 (inc depth))]
+      (doseq [col (range (min left xmin) (inc (max xmax right)))]
+        (cond
+          (wall? [col row]) (print "#")
+          (sand? [col row]) (print "o")
+          :else (print ".")))
+      (println))))
+
+(comment
+  (require '[aoc2022.day14-test :refer [example]])
+  (-> (iterate sand (initial-state (parse-input example)))
+      (nth 24)
+      viz)
+  (-> (iterate sand (add-floor (initial-state (parse-input example))))
+      (nth 93)
+      viz))
--- test/aoc2022/day14_test.clj
+++ test/aoc2022/day14_test.clj
@@ -3,14 +3,15 @@
             [aoc2022.day14 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example "498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9")
 
 (deftest works
-  (testing "with example input")
-    (is (nil? (solve-part1 example-input)))
-    (is (nil? (solve-part2 example-input)))
+  (testing "with example input"
+    (is (= 24 (solve-part1 (parse-input example))))
+    (is (= 93 (solve-part2 (parse-input example)))))
 
-  (testing "with real input")
+  (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day14.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input)))))
+        (is (= 1133 (solve-part1 input)))
+        (is (= 27566 (solve-part2 input))))))
