93f733dcd2ae188ec33df36be510d9c3b8feb305
David Wagner <wagdav@gmail.com>
2022-12-18 10:03:24 +0100

    Solve day18 part1

--- src/aoc2022/day18.clj
+++ src/aoc2022/day18.clj
@@ -1,10 +1,97 @@
 (ns aoc2022.day18
-  (:require [clojure.string :as str]))
+  (:require [aoc2022.search :as search]))
 
 (defn parse-input [input]
   (->> (re-seq #"-?\d+" input)
-  (mapv #(Integer/parseInt %))))
+       (mapv #(Integer/parseInt %))
+       (partition 3)
+       (mapv vec)
+       set))
 
-(defn solve-part1 [input])
+(def faces [[ 1  0  0]
+            [-1  0  0]
+            [ 0  1  0]
+            [ 0 -1  0]
+            [ 0  0  1]
+            [ 0  0 -1]])
 
-(defn solve-part2 [input])
+(defn add [x y]
+  (mapv + x y))
+
+(defn adjacent [p]
+  (map (partial add p) faces))
+
+(defn manhattan [p1 p2]
+  (->> (map - p1 p2)
+       (map #(Math/abs ^long %))
+       (apply +)))
+
+(defn shortest-path [cubes start end]
+  (search/uniform-cost
+    (reify search/Problem
+      (actions [_ state]
+        (->> (adjacent state)
+             (remove cubes)))
+      (goal? [_ state]
+        (= state end))
+      (initial-state [_]
+        (assert (nil? (cubes start)) "Should start from an empty cube")
+        start)
+      (result [_ _ action]
+        action)
+      (step-cost [_ state action]
+        (manhattan state end)))))
+
+(defn weight [cubes]
+  (->> cubes
+       (map (fn [c]
+              (into {c 6} (for [n (adjacent c)] [n -1]))))
+       (apply merge-with +)))
+
+(defn weight-outside [cubes]
+  (->> cubes
+       (map (fn [c]
+                (into {c 6} (for [n (adjacent c)]
+                              (if (cubes n)
+                                [n -1]
+                                (if (shortest-path cubes n [0 0 0])
+                                  [n 1]
+                                  [c 0]))))))
+       (apply merge-with +)))
+
+(defn solve-part1 [cubes]
+  (->> (weight cubes)
+       (vals)
+       (filter pos?)
+       (apply +)))
+
+(defn solve-part2 [input]
+  #_(->> (weight-outside input)
+         (vals)
+         (filter pos?)
+         (apply +)))
+
+(defn viz [coords]
+  (let [lava? (set coords)
+        sz 20]
+    (doseq [z (range sz -1 -1)]
+      (println "z=" z)
+      (doseq [row (range sz)]
+        (doseq [col (range sz)]
+          (cond
+            (lava? [col row z]) (print "o")
+            :else (print ".")))
+        (println))
+      (println))))
+
+(comment
+ (def ex (parse-input example))
+ (def real (parse-input (slurp (clojure.java.io/resource "day18.txt"))))
+ (adjacent [10 10 10])
+ (ex [2 2 2])
+ (shortest-path ex [2 2 6] [1 1 1])
+ (shortest-path ex [2 2 5] [0 0 1])
+ (viz (parse-input (slurp (clojure.java.io/resource "day18.txt"))))
+ (viz ex)
+ (solve-part2 ex)
+ (solve-part2 real))
--- test/aoc2022/day18_test.clj
+++ test/aoc2022/day18_test.clj
@@ -3,14 +3,26 @@
             [aoc2022.day18 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example "2,2,2
+1,2,2
+3,2,2
+2,1,2
+2,3,2
+2,2,1
+2,2,3
+2,2,4
+2,2,6
+1,2,5
+3,2,5
+2,1,5
+2,3,5")
 
 (deftest works
-  (testing "with example input")
-    (is (nil? (solve-part1 example-input)))
-    (is (nil? (solve-part2 example-input)))
+  (testing "with example input"
+    (is (= 64 (solve-part1 (parse-input example))))
+    (is (= 58 (solve-part2 (parse-input example)))))
 
-  (testing "with real input")
+  (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day18.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input)))))
+      (is (= 3396 (solve-part1 input)))
+      (is (nil? (solve-part2 input))))))  ; 3252, 2847, 2698 too high
