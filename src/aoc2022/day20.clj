b209952a4dca164b39af8a0403a582ce62f17ccc
David Wagner <wagdav@gmail.com>
2022-12-20 13:53:09 +0100

    Solve day 20

--- src/aoc2022/day20.clj
+++ src/aoc2022/day20.clj
@@ -1,10 +1,52 @@
-(ns aoc2022.day20
-  (:require [clojure.string :as str]))
+(ns aoc2022.day20)
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-  (mapv #(Integer/parseInt %))))
+   (mapv #(Integer/parseInt %)) (re-seq #"-?\d+" input))
 
-(defn solve-part1 [input])
+(defn move
+  "Move the i-th element in the sequence by n"
+  [s i n]
+  (let [remaining (dec (count s))]
+    (conj
+      (->> (cycle s)
+           (drop i)
+           (take (count s))
+           (rest)
+           (cycle)
+           (drop (mod n remaining))
+           (take remaining)
+           (vec))
+      (nth s i))))
 
-(defn solve-part2 [input])
+(defn mix
+ ([input]
+  (mix input (range (count input))))
+ ([input p]
+  (reduce
+    (fn [res n]
+      (let [idx (.indexOf res n)
+            amount (input n)]
+        (move res idx amount)))
+    (vec p)
+    (range (count input)))))
+
+(defn permutation [input p]
+  (vec (for [i p] (get input i))))
+
+(defn grove-coordinates [input p]
+  (let [m (permutation input p)]
+    (apply +
+      (for [i [1000 2000 3000]]
+        (nth (drop-while #(not= 0 %) (cycle m))
+             i)))))
+
+(defn solve-part1 [input]
+  (grove-coordinates input (mix input)))
+
+(defn solve-part2 [input]
+  (let [decryption-key 811589153
+        real-input (mapv #(* decryption-key %) input)
+        p (nth (iterate (partial mix real-input)
+                        (range (count input)))
+               10)]
+    (grove-coordinates real-input p)))
--- test/aoc2022/day20_test.clj
+++ test/aoc2022/day20_test.clj
@@ -1,16 +1,22 @@
 (ns aoc2022.day20-test
   (:require [clojure.test :refer [deftest is testing]]
-            [aoc2022.day20 :refer [parse-input solve-part1 solve-part2]]
+            [aoc2022.day20 :refer [lookup mix parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example "1
+2
+-3
+3
+-2
+0
+4")
 
 (deftest works
-  (testing "with example input")
-    (is (nil? (solve-part1 example-input)))
-    (is (nil? (solve-part2 example-input)))
+  (testing "with example input"
+    (is (= 3 (solve-part1 (parse-input example))))
+    (is (= 1623178306 (solve-part2 (parse-input example)))))
 
-  (testing "with real input")
+  (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day20.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input)))))
+      (is (= 11037 (solve-part1 input)))
+      (is (= 3033720253914 (solve-part2 input))))))
