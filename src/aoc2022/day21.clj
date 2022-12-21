2247c445a9db818786106902250af44ea09aa75f
David Wagner <wagdav@gmail.com>
2022-12-21 07:59:14 +0100

    Solve day21

--- src/aoc2022/day21.clj
+++ src/aoc2022/day21.clj
@@ -1,10 +1,70 @@
 (ns aoc2022.day21
   (:require [clojure.string :as str]))
 
+(defn parse-line [line]
+  (let [[n c] (str/split line #": ")
+        [a op b] (str/split c #" ")]
+    (if op
+      [n {:op ({"+" + "-" - "/" quot "*" *} op) :args [a b]}]
+      [n (Integer/parseInt a)])))
+
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-  (mapv #(Integer/parseInt %))))
+  (->> (str/split-lines input)
+       (mapv parse-line)
+       (into {})))
 
-(defn solve-part1 [input])
+(defn evaluate [nodes start]
+  (loop [visited {} stack [start]]
+    (if-some [n (peek stack)]
+      (let [{:keys [op args]} (nodes n)]
+        (cond
+          (and op (every? visited args))
+          (recur (assoc visited n (apply op (for [arg args] (visited arg))))
+                 (pop stack))
 
-(defn solve-part2 [input])
+          op
+          (recur visited (into stack args))
+
+          :else
+          (recur (assoc visited n (nodes n))
+                 (pop stack))))
+      ; stack is empty
+      (visited start))))
+
+(defn solve-part1 [input]
+  (evaluate input "root"))
+
+(defn fix-root
+  "For part 2, use `compare` at the root node and arrange the arguments such
+  that the left subtree is smaller than the right subtree."
+  [input]
+  {:post [#(pos? (evaluate % "root"))]}
+  (let [with-compare (assoc-in input ["root" :op] compare)]
+    (if (neg? (evaluate with-compare "root"))
+      (update-in with-compare ["root" :args] reverse)
+      with-compare)))
+
+(defn yell [input h]
+  (assoc input "humn" h))
+
+(defn bounds [input]
+  (let [upper (loop [h (input "humn")]
+                (if (pos? (evaluate (yell input h) "root"))
+                  (recur (* 10 h))
+                  h))
+        lower (quot upper 10)]
+    [lower upper]))
+
+(defn bisect [f [lo hi]]
+  {:pre [(< lo hi) (= -1 (* (f lo) (f hi)))]}
+  (loop [lo lo hi hi]
+    (let [c (quot (+ lo hi) 2)]
+      (case (* (f lo) (f c))
+         0  c
+         1  (recur c hi)
+        -1  (recur lo c)))))
+
+(defn solve-part2 [input]
+  (let [fixed (fix-root input)]
+    (bisect #(evaluate (yell fixed %) "root")
+            (bounds fixed))))
--- test/aoc2022/day21_test.clj
+++ test/aoc2022/day21_test.clj
@@ -3,14 +3,28 @@
             [aoc2022.day21 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example "root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32")
 
 (deftest works
-  (testing "with example input")
-    (is (nil? (solve-part1 example-input)))
-    (is (nil? (solve-part2 example-input)))
+  (testing "with example input"
+    (is (= 152 (solve-part1 (parse-input example))))
+    (is (= 301 (solve-part2 (parse-input example)))))
 
-  (testing "with real input")
-    (let [input (parse-input (slurp (io/resource "day21.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input)))))
+  (testing "with real input"
+    (time (let [input (parse-input (slurp (io/resource "day21.txt")))]
+            (is (= 84244467642604 (solve-part1 input)))
+            (is (= 3759569926192 (solve-part2 input)))))))
