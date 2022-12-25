1a57f51274792392d46a28ca7b66589430fff2da
David Wagner <wagdav@gmail.com>
2022-12-25 10:19:47 +0100

    Solve day25

--- src/aoc2022/day25.clj
+++ src/aoc2022/day25.clj
@@ -1,10 +1,32 @@
 (ns aoc2022.day25
   (:require [clojure.string :as str]))
 
+(def snafu->values {\2 2, \1 1, \0 0, \- -1, \= -2})
+(def values->snafu {2 \2, 1 \1, 0 \0, -1 \-, -2 \=})
+
+(defn snafu->decimal [snafu]
+  (long
+    (apply +
+      (map (fn [s i]
+             (* (snafu->values s)
+                (Math/pow 5 i)))
+           (reverse snafu)
+           (range)))))
+
+(defn decimal->snafu [decimal]
+  (loop [res '()
+         v decimal]
+    (if (<= v 2)
+      (->> (conj res v)
+           (map values->snafu)
+           (str/join))
+      (let [d (-> v (+ 2) (mod 5) (- 2))]
+        (recur (conj res d) (quot (+ 2 v) 5))))))
+
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-  (mapv #(Integer/parseInt %))))
+  (str/split-lines input))
 
-(defn solve-part1 [input])
-
-(defn solve-part2 [input])
+(defn solve-part1 [input]
+  (->> (map snafu->decimal input)
+       (apply +)
+       (decimal->snafu)))
--- test/aoc2022/day25_test.clj
+++ test/aoc2022/day25_test.clj
@@ -3,14 +3,24 @@
             [aoc2022.day25 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example "1=-0-2
+12111
+2=0=
+21
+2=01
+111
+20012
+112
+1=-1=
+1-12
+12
+1=
+122")
 
 (deftest works
-  (testing "with example input")
-    (is (nil? (solve-part1 example-input)))
-    (is (nil? (solve-part2 example-input)))
+  (testing "with example input"
+    (is (= "2=-1=0" (solve-part1 (parse-input example)))))
 
-  (testing "with real input")
+  (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day25.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input)))))
+      (is (= "2-02===-21---2002==0" (solve-part1 input))))))
